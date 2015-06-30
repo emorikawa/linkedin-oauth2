461d083f66763ab66805c68120b2d72e83106f33
Harper Maddox <harpermaddox@gmail.com>
2015-06-30 17:03:48 -0500

    Extended the raise_error class to throw specific errors on a 400 or a 403.  The usage of Faraday was hiding the REST error messages from LinkedIn and it was difficult for me to debug my app.  Exposing the errors should help others get their apps working.

--- lib/linked_in/raise_error.rb
+++ lib/linked_in/raise_error.rb
@@ -5,7 +5,17 @@ module LinkedIn
     def on_complete(response)
       status_code = response.status.to_i
       if status_code == 403 && response.body =~ /throttle/i
-        raise LinkedIn::ThrottleError
+        fail LinkedIn::ThrottleError
+      elsif [400,403].include?(status_code)
+        body = MultiJson.load(response.body)
+
+        if status_code == 400 && response.body =~ /is missing/i        
+          fail LinkedIn::ArgumentError, body['message'] || LinkedIn::ErrorMessages.argument_missing
+        elsif status_code == 400
+          fail LinkedIn::InvalidRequest, body['message'] || LinkedIn::ErrorMessages.arguments_malformed
+        else # status_code == 403
+          fail LinkedIn::PermissionsError, body['message'] || LinkedIn::ErrorMessages.not_permitted
+        end
       else
         super
       end
