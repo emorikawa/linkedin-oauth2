88ce3972b31f16113161eb7da884cfe24de52d55
Cameron Stanley <cameronstanley23@gmail.com>
2020-03-28 08:18:53 -0500

    Adds retrieve organization endpoint and adds localized description to ACLs response

--- lib/linked_in/api.rb
+++ lib/linked_in/api.rb
@@ -54,6 +54,7 @@ module LinkedIn
     def_delegators :@share_and_social_stream, :add_text_share
 
     def_delegators :@organizations, :organization_access
+                                    :organization
 
     private ##############################################################
 
--- lib/linked_in/organizations.rb
+++ lib/linked_in/organizations.rb
@@ -1,11 +1,25 @@
 module LinkedIn
 
   class Organizations < APIResource
+
     # Retrieves a member's organization access control information
     def organization_access(options={})
-      path = "/organizationAcls?q=roleAssignee"
+      path = "/organizationAcls?q=roleAssignee&projection=(elements*(*,organization~(localizedName)))"
       get(path, options)
     end
-  end
+
+    # Retrieves an organization
+    def organization(options={})
+      path = organization_path(options)
+      get(path, options)
+    end
+
+    def organization_path(options)
+      path = "/organizations"
+
+      if id = options.delete(:id)
+        path += "/#{id}"
+      end
+    end
 
 end
